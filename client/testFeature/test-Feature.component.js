569bc504f6c0c4a37aeef18e1f7f92b6d4a8f56b
nick_d <ngdaniele@gmail.com>
2016-10-22 12:02:43 -0700

    Completed front-end post request for test data

--- client/testFeature/test-Feature.component.js
+++ client/testFeature/test-Feature.component.js
@@ -1,10 +1,23 @@
 angular.module('testFeature')
 .component('testFeature', {
   templateUrl: '../testFeature/test-Feature.template.html',
-  controller: function testFeatureController() {
+  controller: ['$http',function testFeatureController($http) {
     this.input = 'default';
     this.sending = function(currentInput) {
-      console.log('Current Input : ', currentInput);
+      console.log('currentInput : ', currentInput);
+      $http({
+        method: 'POST',
+        url: '/test',
+        data: JSON.stringify(currentInput)
+      }).then(function successCallback(response) {
+          // this callback will be called asynchronously
+          // when the response is available
+          console.log('POST request was a success!');
+      }, function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+          console.log('POST request failed!');
+      });
     }
-  }
+  }]
 });
